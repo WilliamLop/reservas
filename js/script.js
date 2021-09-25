425030f97f4f4145c9531d9e391bc3cf129d6ccb
William <calderonwilliam25@gmail.com>
2021-09-24 20:02:45 -0500

    app reserva hotel

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function (){
+    $("#datepicker_llegada").datepicker();
+    $("#datepicker_salida").datepicker();
+
+    $("#formulario").on("submit", function (event) {
+        event.preventDefault();
+
+        let llegada = $("#datepicker_llegada").val();
+        let salida = $("#datepicker_salida").val();
+        localStorage.setItem("llegada", llegada);
+         localStorage.setItem("salida", salida);
+
+        window.location.replace("confirmacion.html");
+    });
+});
+
+
+function clickLugar(lugar){
+    localStorage.setItem("lugar", lugar.value);
+}
+
+function clickCantidadPersonas(cantidadPresonas){
+    localStorage.setItem("cantidadPersonas", cantidadPresonas.value);
+    
+}
